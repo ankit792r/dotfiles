fe5e66a3d135b71f6adb8a411e93f5b6b97b09b4
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-10-29 15:04:32 +0000

    added tiny diagnostics

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,11 @@
 return {
   -- disable trouble
   { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.diagnostics = opts.diagnostics or {}
+      opts.diagnostics.virtual_text = false
+    end,
+  },
 }
